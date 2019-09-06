83baacfa7d613d5731d72aba493155bda423b544
sobhanessi <sobhanessi@gmail.com>
2019-09-06 22:03:18 +0300

    a little bit stylish change in sign up page

---
+++ accounts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+
+
+class SignUpView(generic.CreateView):
+	
+	form_class = UserCreationForm
+	template_name = 'registration/signup.html'
+	success_url = reverse_lazy('home')
