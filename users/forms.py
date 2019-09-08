40c9e18d27106df1aa1fdf01c7125c41b7fb17f4
sobhanessi <sobhanessi@gmail.com>
2019-09-08 17:24:27 +0300

    fixed the problems with bootstrap and css in login and signup pages

--- users/forms.py
+++ users/forms.py
@@ -5,6 +5,12 @@ from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
 	
+	username = forms.CharField(widget=forms.TextInput(attrs={'class':"form-control"}))
+	email	 = forms.EmailField(widget=forms.EmailInput(attrs={'class':"form-control"}))
+	password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':"form-control"}))
+	password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':"form-control"}))
+	age = forms.IntegerField(widget=forms.NumberInput(attrs={'class':"form-control"}))
+	
 	class Meta:
 		
 		model = CustomUser
@@ -12,6 +18,10 @@ class CustomUserCreationForm(UserCreationForm):
 		
 		
 class CustomUserChangeForm(UserChangeForm):
+	
+	username = forms.CharField(widget=forms.TextInput(attrs={'class':"form-control"}))
+	email	 = forms.EmailField(widget=forms.EmailInput(attrs={'class':"form-control"}))
+	password = forms.CharField(widget=forms.PasswordInput(attrs={'class':"form-control"}))
 	
 	class Meta:
 		
