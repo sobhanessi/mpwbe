c2e3053e87dafc65a71e8cebf3fe28e0c0e8ae33
sobhanessi <sobhanessi@gmail.com>
2019-09-10 19:28:41 +0300

    I could finally add comments in my page and I'm so happy

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import PreNews2 #, NewsCreateTM
+from .models import PreNews2 , Comment
 # Register your models here.
 
-admin.site.register(PreNews2)
-# ~ admin.site.register(NewsCreateTM)
+
+
+class CommentInline(admin.TabularInline):
+	
+	model = Comment
+
+class PreNews2Admin(admin.ModelAdmin):
+	
+	inlines = [ CommentInline, ]
+
+
+admin.site.register(PreNews2, PreNews2Admin)
+admin.site.register(Comment)
--- news/models.py
+++ news/models.py
@@ -5,48 +5,6 @@ from django.contrib.auth import get_user_model
 from pages.models import LoginFormModel
 from django.urls import reverse
 
-# ~ class PreNews(models.Model):
-	
-	# ~ hardware = 'chw'
-	# ~ software = 'csw'
-	# ~ politics_iran = 'pir'
-	# ~ politics_international = 'pin'
-	# ~ news_choice = [
-		# ~ (hardware,'computer hardware news'),
-		# ~ (software,'computer software news'),
-		# ~ (politics_iran,'iran politics'),
-		# ~ (politics_international,'international politics'),
-			# ~ ]
-	
-	# ~ #author = models.ForeignKey('settings.AUTH_USER_MODEL', on_delete = models.CASCADE ,)
-	# ~ title = models.TextField(max_length=100,null=False)
-	# ~ tags = models.TextField(default='sobhan esfandyari,')
-	# ~ choice = models.CharField(max_length=3 , choices=news_choice , default=software)
-	# ~ slug = models.SlugField(unique=True,blank=False,null=False)
-	# ~ date = models.DateTimeField(auto_now_add=True)
-	# ~ main_pic = models.ImageField(upload_to='images/',null=False)
-	# ~ brief = models.TextField(max_length=255,null=False)
-	# ~ article = models.TextField(null=False)
-	
-	# ~ def __str__(self):
-		# ~ return self.title
-	
-	# ~ def get_absolute_url(self):
-		# ~ return reverse('sub_news_url', kwargs = { 'slug' : self.slug ,'year' : self.date.year , 'month' : self.date.month , 'day' : self.date.day })
-		
-# ~ class NewsCreateTM(models.Model):
-	
-	# ~ author = models.ForeignKey('auth.User' , on_delete = models.CASCADE,)
-	# ~ title  = models.TextField()
-	# ~ slug   = models.SlugField(unique=True,blank=False,null=False)
-	# ~ pic	   = models.ImageField(upload_to='images/')
-	
-	# ~ def __str__(self):
-		# ~ return self.title
-		
-	# ~ def get_absolute_url(self):
-		# ~ return reverse('sub_news_url', kwargs = { 'slug' : self.slug })
-
 class PreNews2(models.Model):
 	
 	hardware = 'chw'
@@ -75,3 +33,16 @@ class PreNews2(models.Model):
 	
 	def get_absolute_url(self):
 		return reverse('sub_news_url', kwargs = { 'slug' : self.slug ,'year' : self.date.year , 'month' : self.date.month , 'day' : self.date.day })
+
+class Comment(models.Model):
+	
+	article = models.ForeignKey( PreNews2 , on_delete = models.CASCADE, related_name = 'comments',)
+	author = models.ForeignKey( get_user_model() , on_delete = models.CASCADE,)
+	comment = models.TextField(max_length=512)
+	
+	def __str__(self):
+		return self.comment
+	
+	def get_absolute_url(self):
+		return reverse('sub_news_url', kwargs = { 'slug' : self.slug ,'year' : self.date.year , 'month' : self.date.month , 'day' : self.date.day })
+	
