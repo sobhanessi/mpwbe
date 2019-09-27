6233ebb9f6dd32a54eff9103295477e11fafd260
sobhanessi <sobhanessi@gmail.com>
2019-09-27 15:33:20 +0300

    changed debug mode to false

--- users/models.py
+++ users/models.py
@@ -5,4 +5,9 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
 	
 	age = models.PositiveIntegerField(null=True,blank=True)
+	# ~ username = models.CharField(max_length=32,unique=True,blank=False,null=False)
+	# ~ email = models.EmailField(_('email address'), unique=True,
 	
+	# ~ USERNAME_FIELD = 'username'
+	# ~ EMAIL_FIELD = 'email'
+	# ~ REQUIRED_FIELDS = []
